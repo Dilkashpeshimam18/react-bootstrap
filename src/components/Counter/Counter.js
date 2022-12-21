b1ee0c1b433c3f9857292bd61d2e7a41179992b1
Dilkash <dilkashpeshimam@gmail.com>
2022-12-21 14:01:05 +0530

    added increment & decrement functionality

--- src/components/Counter/Counter.js
+++ src/components/Counter/Counter.js
@@ -2,7 +2,13 @@ import React, { useState } from 'react'
 import { Button, Card, Col, Container, Row } from 'react-bootstrap'
 
 const Counter = () => {
-
+    const [count, setCount] = useState(0)
+    const handleIncrement = () => {
+        setCount(prev => prev + 1)
+    }
+    const handleDecrement = () => {
+        setCount(prev => prev - 1)
+    }
     return (
         <>
             <Container className='mt-5'>
@@ -11,9 +17,9 @@ const Counter = () => {
                         <Card className='shadow-lg'>
                             <Card.Body>
                                 <p className='display-6'>Counter</p>
-                                <p>0</p>
-                                <Button variant='warning' className='m-1'>Increment</Button>
-                                <Button variant='success'>Decrement</Button>
+                                <p>{count}</p>
+                                <Button onClick={handleIncrement} variant='warning' className='m-1'>Increment</Button>
+                                <Button onClick={handleDecrement} variant='success'>Decrement</Button>
                             </Card.Body>
                         </Card>
                     </Col>
